8ec03597be6c10cc68626aa87af5b9072b14dead
AntonZinchenko32 <zinchenko309@gmail.com>
2023-03-07 06:11:27 +0200

    last update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchImages } from './fetchImages';
 
@@ -14,7 +17,10 @@ function handleSubmit(event) {
   } = event.currentTarget;
 
     fetchImages(searchQuery.value)
-    .then((searchResults) => renderImages(searchResults.hits))
+        .then((searchResults) => {
+            renderImages(searchResults.hits);
+            new SimpleLightbox('.gallery a',{captionDelay: "250"});
+        })
     .catch((error) => console.log(error));
 
     event.currentTarget.reset();
@@ -24,32 +30,36 @@ function handleSubmit(event) {
 function renderImages(images) {
     let markup;
 
+    console.log(images);
+
     if (images.length != 0) {
         cardSet.innerHTML = "";
-    markup = images.map(({webformatURL, tags, likes, views, comments, downloads}) => {
-            return `
-            <div class="photo-card">
-                <img src=${webformatURL} alt=${tags} loading="lazy" />
-                <div class="info">
-                    <p class="info-item">
-                        <b>Likes: ${likes}</b>
-                    </p>
-                    <p class="info-item">
-                        <b>Views: ${views}</b>
-                    </p>
-                    <p class="info-item">
-                        <b>Comments: ${comments}</b>
-                    </p>
-                    <p class="info-item">
-                        <b>Downloads: ${downloads}</b>
-                    </p>
+        markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+            <a class="link" href=${largeImageURL}>
+                <div class="photo-card">
+                    <img class="image" src=${webformatURL} alt=${tags} loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes: ${likes}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Views: ${views}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Comments: ${comments}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads: ${downloads}</b>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        `;
+            </a>
+            `;
         }).join("");
     
     cardSet.innerHTML = markup;
     }
 
-else Notify.info('Sorry, there are no images matching your search query. Please try again.');
+    else Notify.info('Sorry, there are no images matching your search query. Please try again.');
 }
